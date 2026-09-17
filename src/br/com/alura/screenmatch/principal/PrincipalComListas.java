5b00b0a22ccab24a9901a8461f910b6ea1ac9080
Gabriel Teixeira <gteixeira.gemeos@gmail.com>
2026-09-17 16:02:34 -0300

    Acrescentar PrincipalComListas

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args){
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item; // aqui esta fazendo um casting, pois na linha de baixo, não é possíveel chamar .etClassificacao(), pois ele não esta na classe Titulo, mas em Filme.
+            // Este casting esta dizendo que item é um filme da classe Filme
+            System.out.println("Classificação " + filme.getClassificacao());
+
+        }
+    }
+}
